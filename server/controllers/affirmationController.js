b59e8f6c98051887bd846efa2c0159f60d812dfc
Celine Adam <celine.adaam@gmail.com>
2023-05-16 14:50:17 +0200

    update

--- server/controllers/affirmationController.js
+++ server/controllers/affirmationController.js
@@ -1,14 +1,13 @@
 import { StatusCodes } from "http-status-codes";
 import Affirmation from "../models/Affirmation.js";
 
-export const listAffirmation = async (req, res) => {
+const listAffirmation = async (req, res) => {
   try {
-    const listAffirmation = await Affirmation.find().maxTimeMS(15000);
-    return res.status(StatusCodes.OK).json(listAffirmation);
+    const listAffirmations = await Affirmation.find().maxTimeMS(15000);
+    return res.status(StatusCodes.OK).json(listAffirmations);
   } catch (error) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ message: error.toString() });
   }
 };
-export default { listAffirmation };
