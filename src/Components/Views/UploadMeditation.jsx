ae4a8a4ba8e3ae7e92646c1ad4cf5ec2b6a959e8
Celine Adam <celine.adaam@gmail.com>
2023-05-28 19:09:06 +0200

    update

--- src/Components/Views/UploadMeditation.jsx
+++ src/Components/Views/UploadMeditation.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import axios from "../../Util/axiosInstance";
 
 export default function UploadMeditation() {
+  const onSubmitForm = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    //POST request to the server with the filedata
+    try {
+      const res = await axios.post("/api/files/create", formData);
+
+      console.log("the response is ", res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div>
-      <form>
+    <div className="upload-section">
+      <form className="upload-form" onSubmit={onSubmitForm}>
         {/* set the name of input to image  */}
         <input type="file" name="image" multiple={false} />
         <button>Upload</button>
