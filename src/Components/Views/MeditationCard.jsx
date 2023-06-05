12c173c98435f344fa223a231d7dda86037c1cca
Celine Adam <celine.adaam@gmail.com>
2023-06-05 13:03:11 +0200

    update card

--- src/Components/Views/MeditationCard.jsx
+++ src/Components/Views/MeditationCard.jsx
@@ -5,17 +5,11 @@ export default function MeditationCard({ card, handelAddPlayList, index }) {
   return (
     <div class="card">
       <div class="card_image">
-        <img src={`api/files/byid/${card.image}`} alt="" />
+        <iframe src={card.link} title="description"></iframe>
       </div>
       <div class="card_content">
         <h2 class="card_title">{card.title}</h2>
         <p class="card_text">{card.description}</p>
-        <audio
-          class="btn card_btn"
-          src={`api/files/byid/${card.audio}`}
-          controls
-          autoPlay
-        />
       </div>
     </div>
   );
