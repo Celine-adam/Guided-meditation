94f36d3a36cb0cab966303335c55986a73fb59fa
Celine Adam <celine.adaam@gmail.com>
2023-07-02 22:23:45 +0200

    add loading dots

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Loading } from "react-loading-dot";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Views/Home";
@@ -6,13 +7,21 @@ import Podcast from "./Components/Views/Podcast";
 import Login from "./Components/Views/Login";
 import Signup from "./Components/Views/Signup";
 import Favorite from "./Components/Views/Favorite";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UploadMeditation from "./Components/Views/UploadMeditation";
 import Journal from "./Components/Views/Journal";
 import CoverPage from "./Components/Views/CoverPage";
 
 function App() {
   const [isAuthorized, setIsAuthorized] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating an asynchronous operation
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   const handleLogin = () => {
     setIsAuthorized(true);
   };
@@ -22,27 +31,36 @@ function App() {
   };
   return (
     <div className="App">
-      {isAuthorized ? (
-        <>
-          <Header handleLogout={handleLogout} />
-
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/podcast" element={<Podcast />} />
-            <Route path="/favorite" element={<Favorite />} />
-            <Route path="/journal" element={<Journal />} />
-            <Route path="/Uploadform" element={<UploadMeditation />} />
-          </Routes>
-        </>
+      {loading ? (
+        <div class="loader"></div> // You can replace this with your Loading component
       ) : (
-        <Routes>
-          <Route path="/" element={<CoverPage />} />
-          <Route
-            path="/signup"
-            element={<Signup handleLogin={handleLogin} />}
-          />
-          <Route path="/login" element={<Login handleLogin={handleLogin} />} />
-        </Routes>
+        <>
+          {isAuthorized ? (
+            <>
+              <Header handleLogout={handleLogout} />
+
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/podcast" element={<Podcast />} />
+                <Route path="/favorite" element={<Favorite />} />
+                <Route path="/journal" element={<Journal />} />
+                <Route path="/Uploadform" element={<UploadMeditation />} />
+              </Routes>
+            </>
+          ) : (
+            <Routes>
+              <Route path="/" element={<CoverPage />} />
+              <Route
+                path="/signup"
+                element={<Signup handleLogin={handleLogin} />}
+              />
+              <Route
+                path="/login"
+                element={<Login handleLogin={handleLogin} />}
+              />
+            </Routes>
+          )}
+        </>
       )}
     </div>
   );
